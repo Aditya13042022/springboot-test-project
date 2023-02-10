fa22ba6f0b811a226b72151462b2d5a203aab93e
Thakur Aditya Singh <adityasing085@gmail.com>
2023-02-10 16:28:50 +0530

    update commit

--- src/main/java/mongo/model/Task.java
+++ src/main/java/mongo/model/Task.java
@@ -18,42 +18,5 @@ public class Task {
     private String assignee;
     private int storyPoint;
 
-    public String getTaskId() {
-        return taskId;
-    }
 
-    public void setTaskId(String taskId) {
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getSeverity() {
-        return severity;
-    }
-
-    public void setSeverity(int severity) {
-        this.severity = severity;
-    }
-
-    public String getAssignee() {
-        return assignee;
-    }
-
-    public void setAssignee(String assignee) {
-        this.assignee = assignee;
-    }
-
-    public int getStoryPoint() {
-        return storyPoint;
-    }
-
-    public void setStoryPoint(int storyPoint) {
-        this.storyPoint = storyPoint;
-    }
 }
